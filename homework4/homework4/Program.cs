cedf259acf853d2524256acd1204d7133eeee44b
xvyang <62140017+serein-dream@users.noreply.github.com>
2021-03-20 00:10:37 +0800

    Add files via upload
    C#第四次作业

---
+++ homework4/homework4/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4_1
+{
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+    //下面是添加的代码！！！！ ForEach(Action<T> action)方法。通过调用这个方法打印链表元素，求最大值、最小值和求和（使用lambda表达式实现）
+    public void Foreach(Action<T> action)
+        {
+            Node<T> f =head;
+            while (f != null)
+            {
+                action(f.Data);
+                f = f.Next;
+            }
+
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 整型List
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            //添加的代码！
+            int minl = intlist.Head.Data;
+            int maxl = intlist.Head.Data;
+            int suml = 0;
+            intlist.Foreach (Console.WriteLine);//输出链表元素
+            intlist.Foreach(thisdata =>
+            {
+                if (minl > thisdata) minl = thisdata;
+            }
+                );//求最小值
+            intlist.Foreach(thisdata =>
+            {
+                if (maxl < thisdata) maxl = thisdata;
+            }
+                );//求和
+            intlist.Foreach(thisdata =>suml+=thisdata );
+            Console.WriteLine($"链表的最大值是：{maxl}");
+            Console.WriteLine($"链表的最小值是：{minl}");
+            Console.WriteLine($"链表的和是：    {suml}");
+            Console.WriteLine("输入任意字符结束：");//避免闪退
+            string s = Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ homework4/homework4_2/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4_2
+{
+
+    public class Timeargs
+    {
+        public int Hour_ { get; set; }
+        public int Minute_ { get; set; }
+        public int Second_ { get; set; }
+    }
+
+    //声明委托类型
+    public delegate void Tickhandler(object sender, Timeargs time);
+    public delegate void Alarmhandler(object sender, Timeargs time);
+
+    public class NormalClock
+    {
+        public int Hour, Minute, Second, Alarm_h, Alarm_m, Alram_s;
+        public event Tickhandler ToTick;
+        public event Alarmhandler ToAlarm;
+
+        public NormalClock(int h, int m, int s)
+        {
+            int t = 1;
+            while (t == 1)
+            {
+                if (h >= 0 && h < 25 && m >= 0 && m < 60 && s >= 0 && s < 60)
+                {
+                    Hour = h;
+                    Minute = m;
+                    Second = s;
+                    t = 0;
+                }
+                else
+                {
+                    Console.WriteLine("输入时间不合常规，请重新输入：");
+                }
+            }
+        }
+
+        public void SetAlarm(int h_,int m_,int s_)
+        {
+            Alarm_h = h_;
+            Alarm_m = m_;
+            Alram_s = s_;
+        }
+
+        public void going()
+        {
+            while (!(Hour == Alarm_h && Minute == Alarm_m && Second == Alram_s))
+            {
+                Second += 1;
+                if (Second == 60)
+                {
+                    Second = 0;
+                    Minute += 1;
+                }
+                if (Minute == 60)
+                {
+                    Minute = 0;
+                    Hour += 1;
+                }
+                if (Hour == 24)
+                {
+                    Hour = 0;
+                }
+                Timeargs time_tick = new Timeargs() { Hour_ = Hour, Minute_ = Minute, Second_ = Second };
+                ToTick(this, time_tick);
+            }
+            Timeargs time_alarm = new Timeargs() { Hour_ = Hour, Minute_ = Minute, Second_ = Second };
+            ToAlarm(this, time_alarm);
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+
+    //事件发布者和监听者区分开
+    public class Myclock
+    {
+        public NormalClock myclock;
+
+        void  ToTick(object sender,Timeargs time)
+        {
+            Console.WriteLine($"Tick!：[ { time.Hour_} : { time.Minute_ } : { time.Second_ }]");
+        }
+        void ToAlarm(object sender, Timeargs time)
+        {
+            Console.WriteLine($"Alarm!：[ { time.Hour_} : { time.Minute_ } : { time.Second_ }]");
+        }
+        public Myclock (int H_my,int M_my,int S_my)
+        {
+            myclock = new NormalClock(H_my, M_my, S_my);
+            myclock.ToTick += ToTick;
+            myclock.ToAlarm += ToAlarm;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int again = 1;
+            while (again == 1)
+            {
+                Console.WriteLine("请依次输入现在的时，分，秒，闹钟的时，分，秒，用回车隔开每个数： ");
+                int Now_h = int.Parse(Console.ReadLine());
+                int Now_m = int.Parse(Console.ReadLine());
+                int Now_s = int.Parse(Console.ReadLine());
+                int Alarm_h = int.Parse(Console.ReadLine());
+                int Alarm_m = int.Parse(Console.ReadLine());
+                int Alarm_s = int.Parse(Console.ReadLine());
+                Myclock myclock = new Myclock(Now_h, Now_m, Now_s);
+                myclock.myclock.SetAlarm(Alarm_h, Alarm_m, Alarm_s);
+                myclock.myclock.going();
+                Console.WriteLine("输入0结束程序，输入1再来一次：");
+                again = int.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}

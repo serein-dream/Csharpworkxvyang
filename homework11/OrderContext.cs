7c5a88c0b37f97f7d93dc3579a4d3478bcc74388
xvyang <62140017+serein-dream@users.noreply.github.com>
2021-05-25 20:34:48 +0800

    Add files via upload

---
+++ homework11/OrderContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework6
+{
+    class OrderContext : DbContext
+    {
+        public OrderContext() : base("OrderDatabase")
+        {
+            Database.SetInitializer(
+            new DropCreateDatabaseIfModelChanges<OrderContext>());
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetails> goods { get; set; }
+
+    }
+}
